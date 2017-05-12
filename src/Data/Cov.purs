48d1dfebdc26356957f30bcb5c05e4db4c79d067
LATBauerdick <github@latbauerdick.de>
2017-05-11 20:51:01 -0500

    refactoring matrix to cov etc

--- src/Data/Cov.purs
+++ src/Data/Cov.purs
@@ -18,18 +18,24 @@ import Data.Maybe (Maybe (..))
 import Data.Monoid.Additive (Additive)
 import Data.Matrix
   ( Matrix (..)
-  , multStd
-  , fromArray, fromArray2, zero_, identity
+  , multStd, tr, scalar
+  , fromArray, fromArray2, zero_, identity, toArray
   ) as M
 import Stuff
 
 newtype Dim3 = Dim3 Int
 newtype Dim5 = Dim5 Int
-newtype Dim53 = Dim53 Int
 
 newtype Cov a = Cov { v :: Array Number }
 newtype Jac a b = Jac { v :: Array Number }
 newtype Vec a = Vec { v :: Array Number }
+type Cov3 = Cov Dim3
+type Cov5 = Cov Dim5
+type Jac53 = Jac Dim5 Dim3
+type Jac35 = Jac Dim3 Dim5
+type Vec3 = Vec Dim3
+type Vec5 = Vec Dim5
+type Jacos = {aa :: Jac53, bb :: Jac53, h0 :: Vec5}
 
 class Mat a where
   val :: a -> Array Number
@@ -37,9 +43,16 @@ class Mat a where
   toMatrix :: a -> M.Matrix Number
 instance matCov3 :: Mat (Cov Dim3) where
   val (Cov {v}) = v
-  fromArray a | A.length a /= 6 =
-                  error $ "Cov3 fromArray: wrong input array length " <> show (A.length a)
-              | otherwise = Cov {v: a}
+  fromArray a | A.length a == 6 = Cov {v: a}
+              | A.length a == 9 = Cov {v: a'} where
+    a11 = unsafePartial $ A.unsafeIndex a 0
+    a12 = unsafePartial $ A.unsafeIndex a 1
+    a13 = unsafePartial $ A.unsafeIndex a 2
+    a22 = unsafePartial $ A.unsafeIndex a 4
+    a23 = unsafePartial $ A.unsafeIndex a 5
+    a33 = unsafePartial $ A.unsafeIndex a 8
+    a' = [a11,a12,a13,a22,a23,a33]
+              | otherwise = error $ "Cov3 fromArray: wrong input array length " <> show (A.length a)
   toMatrix (Cov {v}) = M.fromArray2 3 3 vv where
     a11 = unsafePartial $ A.unsafeIndex v 0
     a12 = unsafePartial $ A.unsafeIndex v 1
@@ -50,9 +63,25 @@ instance matCov3 :: Mat (Cov Dim3) where
     vv = [a11, a12, a13, a12, a22, a23, a13, a23, a33]
 instance matCov5 :: Mat (Cov Dim5) where
   val (Cov {v}) = v
-  fromArray a | A.length a /= 15 =
-                  error "Cov5 fromArray: wrong input array length"
-              | otherwise = Cov {v: a}
+  fromArray a | A.length a == 15 = Cov {v: a}
+              | A.length a == 25 = Cov {v: a'} where
+    a11 = unsafePartial $ A.unsafeIndex a 0
+    a12 = unsafePartial $ A.unsafeIndex a 1
+    a13 = unsafePartial $ A.unsafeIndex a 2
+    a14 = unsafePartial $ A.unsafeIndex a 3
+    a15 = unsafePartial $ A.unsafeIndex a 4
+    a22 = unsafePartial $ A.unsafeIndex a 6
+    a23 = unsafePartial $ A.unsafeIndex a 7
+    a24 = unsafePartial $ A.unsafeIndex a 8
+    a25 = unsafePartial $ A.unsafeIndex a 9
+    a33 = unsafePartial $ A.unsafeIndex a 12
+    a34 = unsafePartial $ A.unsafeIndex a 13
+    a35 = unsafePartial $ A.unsafeIndex a 14
+    a44 = unsafePartial $ A.unsafeIndex a 18
+    a45 = unsafePartial $ A.unsafeIndex a 19
+    a55 = unsafePartial $ A.unsafeIndex a 24
+    a' = [a11,a12,a13,a14,a15,a22,a23,a24,a25,a33,a34,a35,a44,a45,a55]
+              | otherwise = error "Cov5 fromArray: wrong input array length"
   toMatrix (Cov {v}) = M.fromArray2 5 5 vv where
     a11 = unsafePartial $ A.unsafeIndex v 0
     a12 = unsafePartial $ A.unsafeIndex v 1
@@ -90,6 +119,12 @@ instance matJac53 :: Mat (Jac Dim5 Dim3) where
                   error "Jac53 fromArray: wrong input array length"
               | otherwise = Jac {v: a}
   toMatrix (Jac {v}) = M.fromArray2 5 3 v
+instance matJac35 :: Mat (Jac Dim3 Dim5) where
+  val (Jac {v}) = v
+  fromArray a | A.length a /= 15 =
+                  error "Jac35 fromArray: wrong input array length"
+              | otherwise = Jac {v: a}
+  toMatrix (Jac {v}) = M.fromArray2 3 5 v
 class SymMat a where
   inv :: a -> a
   invMaybe :: a -> Maybe a
@@ -102,41 +137,31 @@ instance symMatCov5 :: SymMat (Cov Dim5) where
   inv a = a
   invMaybe a = Just a
   det a = 1.0
-type Cov3 = Cov Dim3
-type Cov5 = Cov Dim5
-
-type Jac53 = Jac Dim5 Dim3
-type Jacos = {aa :: Jac53, bb :: Jac53, h0 :: Vec5}
-
-type Vec3 = Vec Dim3
-type Vec5 = Vec Dim5
-
-sw5 :: Jac53 -> Cov3 -> Cov5
-sw5 j c = one
-sw3 :: Jac53 -> Cov5 -> Cov3
-sw3 j c = one
-swv5 :: Jac53 -> Vec3 -> Vec5
-swv5 j v = zeroVec5
 
 
-sw1 :: Vec3 -> Cov3 -> Number
-sw1 v c = 1.0
-
-tr :: Jac53 -> Jac53
-tr j = j
+tr :: Jac53 -> Jac35
+tr (Jac {v}) = Jac {v:v'} where
+  a11 = unsafePartial $ A.unsafeIndex v 0
+  a12 = unsafePartial $ A.unsafeIndex v 1
+  a13 = unsafePartial $ A.unsafeIndex v 2
+  a14 = unsafePartial $ A.unsafeIndex v 3
+  a15 = unsafePartial $ A.unsafeIndex v 4
+  a21 = unsafePartial $ A.unsafeIndex v 5
+  a22 = unsafePartial $ A.unsafeIndex v 6
+  a23 = unsafePartial $ A.unsafeIndex v 7
+  a24 = unsafePartial $ A.unsafeIndex v 8
+  a25 = unsafePartial $ A.unsafeIndex v 9
+  a31 = unsafePartial $ A.unsafeIndex v 10
+  a32 = unsafePartial $ A.unsafeIndex v 11
+  a33 = unsafePartial $ A.unsafeIndex v 12
+  a34 = unsafePartial $ A.unsafeIndex v 13
+  a35 = unsafePartial $ A.unsafeIndex v 14
+  v' = [a11,a21,a31,a12,a22,a32,a13,a23,a33,a14,a24,a34,a15,a25,a35]
 
 instance semiringCov3 :: Semiring (Cov Dim3) where
   add (Cov {v: v1}) (Cov {v: v2}) = Cov {v: A.zipWith (+) v1 v2}
   zero = Cov {v: A.replicate 6 0.0 }
-  mul (Cov {v: v1}) (Cov {v: v2}) = Cov {v: cov3StdMult v1 v2}
-  one = Cov { v: [1.0, 0.0, 0.0, 1.0, 0.0, 1.0] }
-instance ringCov3 :: Ring (Cov Dim3) where
-  sub (Cov {v: v1}) (Cov {v: v2}) = Cov {v: A.zipWith (-) v1 v2}
-instance showCov3 :: Show (Cov Dim3) where
-  show (Cov {v}) = "Show (Cov Dim3) \n" <> (show $ cov3Matrix v)
-
-cov3StdMult :: Array Number -> Array Number -> Array Number
-cov3StdMult a b = c where
+  mul (Cov {v: a}) (Cov {v: b}) = Cov {v: c} where
     a11 = unsafePartial $ A.unsafeIndex a 0
     a12 = unsafePartial $ A.unsafeIndex a 1
     a13 = unsafePartial $ A.unsafeIndex a 2
@@ -159,29 +184,16 @@ cov3StdMult a b = c where
     --  , a13*b12 + a23*b22 + a33*b23
         , a13*b13 + a23*b23 + a33*b33
         ]
-cov3Matrix :: Array Number -> M.Matrix Number
-cov3Matrix v = M.fromArray2 3 3 m where
-  a11 = unsafePartial $ A.unsafeIndex v 0
-  a12 = unsafePartial $ A.unsafeIndex v 1
-  a13 = unsafePartial $ A.unsafeIndex v 2
-  a22 = unsafePartial $ A.unsafeIndex v 3
-  a23 = unsafePartial $ A.unsafeIndex v 4
-  a33 = unsafePartial $ A.unsafeIndex v 5
-  m = [a11, a12, a13, a12, a22, a23, a13, a23, a33]
+  one = Cov { v: [1.0, 0.0, 0.0, 1.0, 0.0, 1.0] }
+instance ringCov3 :: Ring (Cov Dim3) where
+  sub (Cov {v: v1}) (Cov {v: v2}) = Cov {v: A.zipWith (-) v1 v2}
+instance showCov3 :: Show (Cov Dim3) where
+  show c = "Show (Cov Dim3) \n" <> (show $ toMatrix c)
 
 instance semiringCov5 :: Semiring (Cov Dim5) where
   add (Cov {v: v1}) (Cov {v: v2}) = Cov {v: A.zipWith (+) v1 v2}
   zero = Cov {v: A.replicate 15 0.0 }
-  mul (Cov {v: v1}) (Cov {v: v2}) = Cov {v: cov5StdMult v1 v2}
-  one = Cov { v: [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0
-            , 0.0, 0.0, 1.0, 0.0, 1.0] }
-instance ringCov5 :: Ring (Cov Dim5) where
-  sub (Cov {v: v1}) (Cov {v: v2}) = Cov {v: A.zipWith (-) v1 v2}
-instance showCov5 :: Show (Cov Dim5) where
-  show (Cov {v}) = "Show (Cov Dim5) \n" <> (show $ cov5Matrix v)
-
-cov5StdMult :: Array Number -> Array Number -> Array Number
-cov5StdMult a b = c where
+  mul (Cov {v: a}) (Cov {v: b}) = Cov {v: c} where
     a11 = unsafePartial $ A.unsafeIndex a 0
     a12 = unsafePartial $ A.unsafeIndex a 1
     a13 = unsafePartial $ A.unsafeIndex a 2
@@ -238,83 +250,144 @@ cov5StdMult a b = c where
    --   , a15*b14 + a25*b45 + a35*b45 + a45*b44 + a55*b45
         , a15*b15 + a25*b55 + a35*b55 + a45*b45 + a55*b55
         ]
-cov5Matrix :: Array Number -> M.Matrix Number
-cov5Matrix v = M.fromArray2 5 5 m where
-  a11 = unsafePartial $ A.unsafeIndex v 0
-  a12 = unsafePartial $ A.unsafeIndex v 1
-  a13 = unsafePartial $ A.unsafeIndex v 2
-  a14 = unsafePartial $ A.unsafeIndex v 3
-  a15 = unsafePartial $ A.unsafeIndex v 4
-  a22 = unsafePartial $ A.unsafeIndex v 5
-  a23 = unsafePartial $ A.unsafeIndex v 6
-  a24 = unsafePartial $ A.unsafeIndex v 7
-  a25 = unsafePartial $ A.unsafeIndex v 8
-  a33 = unsafePartial $ A.unsafeIndex v 9
-  a34 = unsafePartial $ A.unsafeIndex v 10
-  a35 = unsafePartial $ A.unsafeIndex v 11
-  a44 = unsafePartial $ A.unsafeIndex v 12
-  a45 = unsafePartial $ A.unsafeIndex v 13
-  a55 = unsafePartial $ A.unsafeIndex v 14
-  m = [ a11, a12, a13, a14, a15
-      , a12, a22, a23, a24, a25
-      , a13, a23, a33, a34, a35
-      , a14, a24, a34, a44, a45
-      , a15, a25, a35, a45, a55
-      ]
+  one = Cov { v: [1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,1.0] }
+instance ringCov5 :: Ring (Cov Dim5) where
+  sub (Cov {v: v1}) (Cov {v: v2}) = Cov {v: A.zipWith (-) v1 v2}
+instance showCov5 :: Show (Cov Dim5) where
+  show c = "Show Cov\n" <> (show $ toMatrix c)
 
--- Instances for Vec
--- almost semiring
-addVec :: forall a.  Vec a -> Vec a -> Vec a
-addVec (Vec {v: v1}) (Vec {v: v2}) = Vec {v: A.zipWith (+) v1 v2}
-zeroVec3 :: Vec3
-zeroVec3 = Vec {v: A.replicate 3 0.0 }
-zeroVec5 :: Vec5
-zeroVec5 = Vec {v: A.replicate 5 0.0 }
-mulVec :: forall a. Vec a ->  Vec a -> Vec a
-mulVec (Vec {v: v1}) (Vec {v: v2}) = Vec {v: A.singleton $ A.foldl (+) zero $ A.zipWith (*) v1 v2}
-scalarVec :: forall a. Vec a -> Number
-scalarVec (Vec {v}) = unsafePartial $ AP.head v
-oneVec3 :: Vec3
-oneVec3 = Vec { v: A.replicate 3 1.0 }
-oneVec5 :: Vec5
-oneVec5 = Vec { v: A.replicate 5 1.0 }
-subVec :: forall a. Vec a -> Vec a -> Vec a
-subVec (Vec {v: v1}) (Vec {v: v2}) = Vec {v: A.zipWith (-) v1 v2}
+instance semiringJac :: Semiring (Jac a b) where
+  add (Jac {v: v1}) (Jac {v: v2}) = Jac {v: A.zipWith (+) v1 v2}
+  zero = undefined -- Jac {v: A.replicate 15 0.0 }
+  mul (Jac {v: v1}) (Jac {v: v2}) = undefined -- Cov {v: cov5StdMult v1 v2}
+  one = undefined -- Cov { v: [1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,1.0] }
+instance ringJac :: Ring (Jac a b) where
+  sub (Jac {v: v1}) (Jac {v: v2}) = Jac {v: A.zipWith (-) v1 v2}
+instance showJac :: Show (Jac a b) where
+  show (Jac {v}) = "Show Jac \n" <> (show $ v)
+
+-- Instances for Vec -- these are always column vectors
+instance semiringVec :: Semiring (Vec a) where
+add (Vec {v: v1}) (Vec {v: v2}) = Vec {v: A.zipWith (+) v1 v2}
+zero = undefined -- Vec {v: A.replicate 3 0.0 }
+                 -- Vec {v: A.replicate 5 0.0 }
+mul (Vec {v: v1}) (Vec {v: v2}) = undefined --Vec {v: A.singleton $ A.foldl (+) zero $ A.zipWith (*) v1 v2}
+--scalarVec :: forall a. Vec a -> Number
+--scalarVec (Vec {v}) = unsafePartial $ AP.head v
+one = undefined -- Vec { v: A.replicate 3 1.0 }
+                -- Vec { v: A.replicate 5 1.0 }
+instance ringVec :: Ring (Vec a) where
+sub (Vec {v: v1}) (Vec {v: v2}) = Vec {v: A.zipWith (-) v1 v2}
 instance showVec3 :: Show (Vec Dim3) where
-  show (Vec {v}) = "Show (Vec Dim3) \n" <> (show $ M.fromArray 3 v)
-instance showVec5 :: Show (Vec Dim5) where
-  show (Vec {v}) = "Show (Vec Dim5) \n" <> (show $ M.fromArray 5 v)
+  show (Vec {v}) = "Show Vec\n" <> (show $ M.fromArray (A.length v) v)
+
 
 class Chi2 a where
   chi2 :: Vec a -> Cov a -> Number
+  sandwichvv :: Vec a -> Vec a -> Number
+infix 7 chi2 as |*|
+infix 7 sandwichvv as |.|
 
 instance chi2Dim5 :: Chi2 Dim5 where
-  chi2 v c = 5.0
+  chi2 v c = x where
+    mv = toMatrix v
+    mc = toMatrix c
+    mx = M.tr mv * mc * mv
+    x = M.scalar mx
+  sandwichvv (Vec {v:v1}) (Vec {v:v2}) = vv where
+    vv = A.foldl (+) zero $ A.zipWith (*) v1 v2
 instance chi2Dim3 :: Chi2 Dim3 where
-  chi2 v c = 3.0
+  chi2 v c = x where
+    mv = toMatrix v
+    mc = toMatrix c
+    mx = M.tr mv * mc * mv
+    x = M.scalar mx
+  sandwichvv (Vec {v:v1}) (Vec {v:v2}) = vv where
+    vv = A.foldl (+) zero $ A.zipWith (*) v1 v2
 
 class TMat a b where
-  sandwich ::  Jac a b -> Cov a -> Cov b
-infixr 6 sandwich as |*|
+  sandwich :: Jac a b -> Cov a -> Cov b
+  transcov :: Jac a b -> Cov b -> Jac a b
+  transvoc :: Cov a -> Jac a b -> Jac a b
+  transvec :: Jac a b -> Vec b -> Vec a
+  sandwichjj :: Jac a b -> Jac b a -> Cov a
+infixr 7 sandwich as ||*||
+infixr 7 transcov as ||*
+infixr 7 transvoc as *||
+infixr 7 transvec as |||
+infixr 7 sandwichjj as ||||
 
 instance tmat35 :: TMat Dim3 Dim5 where
-  sandwich j c = fromArray [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0]
-  {-- (|*|) c j = c --}
+  sandwich j c = c' where
+    mj = toMatrix j
+    mc = toMatrix c
+    mc' = M.tr mj * mc * mj
+    c' = fromArray $ M.toArray mc'
+  transcov j c = j' where
+    mj = toMatrix j
+    mc = toMatrix c
+    mj' = mj * mc
+    j' = fromArray $ M.toArray mj'
+  transvoc c j = j' where
+    mj = toMatrix j
+    mc = toMatrix c
+    mj' = mc * mj
+    j' = fromArray $ M.toArray mj'
+  transvec j v = v' where
+    mj = toMatrix j
+    mv = toMatrix v
+    mv' = mj * mv
+    v' = fromArray $ M.toArray mv'
+  sandwichjj j1 j2 = c' where
+    mj1 = toMatrix j1
+    mj2 = toMatrix j2
+    mc' = mj1 * mj2
+    c' = fromArray $ M.toArray mc'
 instance tmat53 :: TMat Dim5 Dim3 where
-  sandwich j c = fromArray [1.0,2.0,3.0,4.0,5.0,6.0]
+  sandwich j c = c' where
+    mj = toMatrix j
+    mc = toMatrix c
+    mc' = M.tr mj * mc * mj
+    c' = fromArray $ M.toArray mc'
+  transcov j c = j' where
+    mj = toMatrix j
+    mc = toMatrix c
+    mj' = mj * mc
+    j' = fromArray $ M.toArray mj'
+  transvoc c j = j' where
+    mj = toMatrix j
+    mc = toMatrix c
+    mj' = mc * mj
+    j' = fromArray $ M.toArray mj'
+  transvec j v = v' where
+    mj = toMatrix j
+    mv = toMatrix v
+    mv' = mj * mv
+    v' = fromArray $ M.toArray mv'
+  sandwichjj j1 j2 = c' where
+    mj1 = toMatrix j1
+    mj2 = toMatrix j2
+    mc' = mj1 * mj2
+    c' = fromArray $ M.toArray mc'
 
-class TVec a b where
-  transvec :: Jac a b -> Vec b -> Vec a
-infixr 6 transvec as *|
-
-instance tvec35 :: TVec Dim3 Dim5 where
-  transvec j v = fromArray [1.0, 2.0, 3.0]
-instance tvec53 :: TVec Dim5 Dim3 where
-  transvec j v = fromArray [1.0, 2.0, 3.0, 4.0, 5.0]
-
-
-class TTVec a where
+class TVec a where
   ttransvec :: Cov a -> Vec a -> Vec a
+infixr 7 ttransvec as *|
+instance tvec3 :: TVec Dim3 where
+  ttransvec c v = v' where
+    mc = toMatrix c
+    mv = toMatrix v
+    mv' = mc * mv
+    v' = fromArray $ M.toArray mv'
+instance tvec5 :: TVec Dim5 where
+  ttransvec c v = v' where
+    mc = toMatrix c
+    mv = toMatrix v
+    mv' = mc * mv
+    v' = fromArray $ M.toArray mv'
+
+
+
 
 
 
@@ -325,19 +398,25 @@ instance showMD :: Show MD where
 testCov = "testCov: " <> show md <> "\n"
                       <> show mm3
                       <> show mm5
-                      <> show (scalarVec (mulVec (addVec v3 v3)  v3))  
-                      <> show tj3
-                      <> show (chi2 v3 c3) where
+                      <> "exp v3 " <> show ( (v3 + v3) |.| v3 ) <> "\n"
+                      <> "tj3 " <> show tj3 <> "vv3 " <> show vv3
+                      <> show (v3 |*| c3) where
   c3 :: Cov3
   c3 = fromArray [1.0,2.0,3.0,4.0,5.0,6.0]
   c5 :: Cov5
   c5 = fromArray [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0]
   v3 :: Vec3
   v3 = fromArray [10.0,11.0,12.0]
+  v5 :: Vec5
+  v5 = fromArray [10.0,11.0,12.0,13.0,14.0]
   j53 :: Jac53
   j53 = fromArray [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0]
   tj3 :: Cov3
-  tj3 = j53 |*| c5
+  tj3 = j53 ||*|| c5
+  vv5 :: Vec5
+  vv5 = j53 ||| v3
+  vv3 :: Vec3
+  vv3 = tr j53 ||| j53 ||| c3 *| v3
 
   m3 :: M.Matrix Number
   m3 = M.fromArray2 3 3 [1.0,2.0,3.0,2.0,4.0,5.0,3.0,5.0,6.0]
--- src/FV/Fit.purs
+++ src/FV/Fit.purs
@@ -18,105 +18,113 @@ import FV.Jacob as J
 data VHMeas = VHMeas { vertex :: XMeas, helices :: Array HMeas }
 helices (VHMeas {helices}) = helices
 data XMeas = XMeas Vec3 Cov3
-data QMeas = QMeas Vec3 Cov3
+instance showXMeas :: Show XMeas where
+  show _ = "XMeas"
+data QMeas = QMeas Vec3 Cov3 Number
 data HMeas = HMeas Vec5 Cov5 Number
 newtype Chi2 = Chi2 Number
-data Prong = Prong { }
+data Prong = Prong
+          { nProng        :: Int
+          , fitVertex     :: XMeas
+          , fitMomenta    :: Array QMeas
+          , fitChi2s      :: Array Chi2
+          , measurements  :: VHMeas
+          }
 
 fit :: VHMeas -> Prong
-fit v = Prong {}--  undefined
-{-- fit vhm = kSmooth vhm <<< kFilter $ vhm --}
+{-- fit v = Prong {}--  undefined --}
+fit vhm = kSmooth vhm <<< kFilter $ vhm
 
-{-- kFilter :: VHMeas -> XMeas --}
-{-- kFilter (VHMeas {vertex, helices}) = foldl kAdd vertex helices --}
+kFilter :: VHMeas -> XMeas
+kFilter (VHMeas {vertex, helices}) = foldl kAdd vertex helices
 
-{-- kAdd :: XMeas -> HMeas -> XMeas --}
-{-- kAdd (XMeas v vv) (HMeas h hh w0) = kAdd' x_km1 p_k x_e q_e 1e6 0 where --}
-{--   x_km1 = XMeas v (inv vv) --}
-{--   p_k   = HMeas h (inv hh) w0 --}
-{--   x_e   = v --}
-{--   q_e   = J.hv2q h v --}
+kAdd :: XMeas -> HMeas -> XMeas
+kAdd (XMeas v vv) (HMeas h hh w0) = kAdd' x_km1 p_k x_e q_e 1e6 0 where
+  x_km1 = XMeas v (inv vv)
+  p_k   = HMeas h (inv hh) w0
+  x_e   = v
+  q_e   = J.hv2q h v
 
-{-- goodEnough :: Number -> Number -> Int -> Boolean --}
-{-- goodEnough c0 c i | i < 3 && trace ("." <> show i <> "|" <> to1fix (abs (c-c0)) <> to1fix c) false = undefined --}
-{-- goodEnough c0 c i = abs (c - c0) < chi2cut || i > iterMax where --}
-{--   chi2cut = 0.5 --}
-{--   iterMax = 99 :: Int --}
+goodEnough :: Number -> Number -> Int -> Boolean
+goodEnough c0 c i | i < 3 && trace ("." <> show i <> "|" <> to1fix (abs (c-c0)) <> to1fix c) false = undefined
+goodEnough c0 c i = abs (c - c0) < chi2cut || i > iterMax where
+  chi2cut = 0.5
+  iterMax = 99 :: Int
 
-{-- -- | add a helix measurement to kalman filter, return updated vertex position --}
-{-- -- | if we can't invert, don't update vertex --}
-{-- kAdd' :: XMeas -> HMeas -> Vec3 -> Vec3 -> Number -> Int -> XMeas --}
-{-- kAdd' (XMeas v0 uu0) (HMeas h gg w0) x_e q_e 𝜒2_0 iter = x_k where --}
-{--   jj    = J.expand x_e q_e --}
-{--   aa    = jj.aa --}
-{--   bb    = jj.bb --}
-{--   h0    = jj.h0 --}
-{--   aaT   = tr aa --}
-{--   bbT   = tr bb --}
-{--   x_k   = case invMaybe (sw3 bb gg) of --}
-{--             Nothing  -> XMeas v0 (inv uu0) -- `debug` "... in kAdd'" --}
-{--             Just ww  -> let --}
-{--                 gb    = gg - gg * (sw5 bbT ww) * gg --}
-{--                 uu    = uu0 + sw3 aa gb --}
-{--                 cc    = inv uu --}
-{--                 m     = h - h0 --}
-{--                 v     = cc * (uu0 * v0 + aaT * gb * m) --}
-{--                 dm    = m - aa * v --}
-{--                 q     = ww * bbT * gg * dm --}
-{--                 𝜒2    = sw1 (dm - bb * q) gg + sw1 (v - v0) uu0 --}
-{--                 x_k'  = if goodEnough 𝜒2_0 𝜒2 iter -- `debug` ("--> kAdd' chi2 is " ++ show 𝜒2) --}
-{--                   then XMeas v cc --}
-{--                   else kAdd' (XMeas v0 uu0) (HMeas h gg w0) v q 𝜒2 (iter+1) --}
-{--               in x_k' --}
+-- | add a helix measurement to kalman filter, return updated vertex position
+-- | if we can't invert, don't update vertex
+kAdd' :: XMeas -> HMeas -> Vec3 -> Vec3 -> Number -> Int -> XMeas
+kAdd' (XMeas v0 uu0) (HMeas h gg w0) x_e q_e 𝜒2_0 iter = x_k where
+  jj    = J.expand x_e q_e
+  aa    = jj.aa
+  bb    = jj.bb
+  h0    = jj.h0
+  aaT   = tr aa
+  bbT   = tr bb
+  x_k   = case invMaybe (bb ||*|| gg) of
+            Nothing  -> XMeas v0 (inv uu0) -- `debug` "... in kAdd'"
+            Just ww  -> let
+                gb    = gg - gg * (bbT ||*|| ww) * gg
+                uu    = uu0 + aa ||*|| gb
+                cc    = inv uu
+                m     = h - h0
+                v     = cc *| (uu0 *| v0 + aaT ||| gb *| m)
+                dm    = m - aa ||| v
+                q     = ww *| bbT ||| gg *| dm
+                𝜒2    = (dm - bb ||| q) |*| gg + (v - v0) |*| uu0
+                x_k'  = if goodEnough 𝜒2_0 𝜒2 iter -- `debug` ("--> kAdd' chi2 is " ++ show 𝜒2)
+                  then XMeas v cc
+                  else kAdd' (XMeas v0 uu0) (HMeas h gg w0) v q 𝜒2 (iter+1)
+              in x_k'
 
-{-- kSmooth :: VHMeas -> XMeas -> Prong --}
-{-- kSmooth vm v | trace ("kSmooth " <> (show <<< length <<< helices $ vm) <> ", vertex at " <> (show v) ) false = undefined --}
-{-- kSmooth (VHMeas {vertex: v0, helices: hl}) v = pr' where --}
-{--   {1-- (Tuple ql chi2l) = unzip $ mapMaybe (ksm v) hl --1} --}
-{--   xx = unzip $ mapMaybe (ksm v) hl --}
-{--   ql = fst xx --}
-{--   chi2l = snd xx --}
-{--   hl' = hl --}
-{--   n = length hl --}
-{--   n' = length ql --}
-{--   n'' = if n == n' then n else n' `debug` "kSmooth killed helices" --}
-{--   pr' = Prong { fitVertex: v, fitMomenta: ql, fitChi2s: chi2l, nProng: n'', measurements: VHMeas {vertex: v0, helices: hl'} } --}
+kSmooth :: VHMeas -> XMeas -> Prong
+kSmooth vm v | trace ("kSmooth " <> (show <<< length <<< helices $ vm) <> ", vertex at " <> (show v) ) false = undefined
+kSmooth (VHMeas {vertex: v0, helices: hl}) v = pr' where
+  {-- (Tuple ql chi2l) = unzip $ mapMaybe (ksm v) hl --}
+  xx = unzip $ mapMaybe (ksm v) hl
+  ql = fst xx
+  chi2l = snd xx
+  hl' = hl
+  n = length hl
+  n' = length ql
+  n'' = if n == n' then n else n' `debug` "kSmooth killed helices"
+  pr' = Prong { fitVertex: v, fitMomenta: ql, fitChi2s: chi2l, nProng: n'', measurements: VHMeas {vertex: v0, helices: hl'} }
 
-{-- -- kalman smoother step: calculate 3-mom q and chi2 at kalman filter'ed vertex --}
-{-- -- if we can't invert, return Nothing and this track will not be included --}
-{-- ksm :: XMeas -> HMeas -> Maybe (Tuple QMeas Chi2) --}
-{-- ksm (XMeas x cc) (HMeas h hh w0) = do --}
-{--   let --}
-{--       {1-- HMeas (V5 h) (M5 hh) w0 = hm --1} --}
-{--       jj = J.expand x (J.hv2q h x) --}
-{--       aa = jj.aa --}
-{--       bb = jj.bb --}
-{--       h0 = jj.h0 --}
-{--       gg   = inv hh --}
-{--   ww <- invMaybe (sw3 bb gg) --}
-{--   let --}
-{--       p    = h - h0 --}
-{--       uu   = inv cc --}
-{--       aaT  = tr aa --}
-{--       bbT  = tr bb --}
-{--       q    = ww *| bbT *| gg *| (p - (aa *| x)) --}
-{--       ee   = - cc * aaT * gg * bb * ww --}
-{--       dd   = ww + sw3 ee uu --}
-{--       r    = p - aa*x - bb*q --}
-{--       ch   = sw1 r gg --}
+-- kalman smoother step: calculate 3-mom q and chi2 at kalman filter'ed vertex
+-- if we can't invert, return Nothing and this track will not be included
+ksm :: XMeas -> HMeas -> Maybe (Tuple QMeas Chi2)
+ksm (XMeas x cc) (HMeas h hh w0) = do
+  let
+      {-- HMeas (V5 h) (M5 hh) w0 = hm --}
+      jj = J.expand x (J.hv2q h x)
+      aa = jj.aa
+      bb = jj.bb
+      h0 = jj.h0
+      gg   = inv hh
+  ww <- invMaybe (bb ||*|| gg)
+  let
+      p    = h - h0
+      uu   = inv cc
+      aaT  = tr aa
+      bbT  = tr bb
+      q    = ww *| bbT ||| gg *| (p - aa ||| x)
+      ee   = - (cc *|| aaT) |||| gg *|| bb ||* ww
+      dd   = ww + ee * uu
+      r    = p - aa ||| x - bb ||| q
+      ch   = r |*| gg
 
-{--       gb   = gg - gg * (sw5 bbT ww) * gg --}
-{--       uu'  =  uu - sw3 aa gb --}
-{--       duu  = det uu' --}
-{--       bad  = duu < 0.0 --}
-{--       cx   = if bad then 1000.0 `debug` ("--> ksm bad" <> show duu <> show uu') --}
-{--                     else cx'' where --}
-{--                       cc'  = inv uu' -- `debug` ("--> ksm " ++ show uu') --}
-{--                       x'   = cc' * (uu*x - aaT * gb * p) --}
-{--                       dx   = x - x' --}
-{--                       cx'  = sw1 dx uu' --}
-{--                       cx'' = if cx' < 0.0 then 2000.0 `debug` ("--> ksm chi2 is " <> show cx' <> ", " <> show ch <> ", " <> show ((max cx' 0.0) + ch)) --}
-{--                                         else cx' --}
-{--       chi2 = cx + ch --}
-{--   pure (Tuple (QMeas q dd w0) chi2) --}
+      gb   = gg - gg * (bbT ||*|| ww) * gg
+      uu'  = uu - aa ||*|| gb
+      duu  = det uu'
+      bad  = duu < 0.0
+      cx   = if bad then 1000.0 `debug` ("--> ksm bad" <> show duu <> show uu')
+                    else cx'' where
+                      cc'  = inv uu' -- `debug` ("--> ksm " ++ show uu')
+                      x'   = cc' *| (uu *| x - aaT ||| gb *| p)
+                      dx   = x - x'
+                      cx'  = dx |*| uu'
+                      cx'' = if cx' < 0.0 then 2000.0 `debug` ("--> ksm chi2 is " <> show cx' <> ", " <> show ch <> ", " <> show ((max cx' 0.0) + ch))
+                                        else cx'
+      𝜒2 = cx + ch
+  pure (Tuple (QMeas q dd w0) (Chi2 𝜒2))
 
